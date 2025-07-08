7752d8e5e8b99fdc6b66006fe7aeabdd92fd9717
Yarno Grenier <127676205+yarno97x@users.noreply.github.com>
2025-07-08 17:43:54 -0400

    Added sample preferences

---
+++ params.py
@@ -0,0 +1,12 @@
+from preferences import Calibre
+
+params = {
+    "MaxTime" : "21:00",
+    "MaxDuration" : 45,
+    "Calibres" : [Calibre.BPLUS, Calibre.A, Calibre.AA]
+}
+
+# print(params.keys())
+# print(params.values())
+# print(params.items())
+# print(params["MaxTime"])
