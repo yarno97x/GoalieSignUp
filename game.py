89bdee0caef824b6caa1b6cd320498ea2b0f482c
Yarno Grenier <127676205+yarno97x@users.noreply.github.com>
2025-07-08 00:49:41 -0400

    Added notification system

--- game.py
+++ game.py
@@ -5,6 +5,18 @@ class Game :
         self.time = time
         self.duration = duration
         self.calibre = calibre
+        self.arena = None
 
-    def __repr__(self):
-        return f"{self.title}, {self.date}, {self.time}, {self.duration}, {self.calibre}"
+    def __repr__(self) :
+        return f"'{self.title}, {self.date}, {self.time}, {self.duration}, {self.calibre}'"
+    
+    def set_arena(self, arena) :
+        self.arena = arena
+
+    def to_html(self) :
+        string = "<div style='border: 1px solid white; padding-left: 10px; margin-bottom: 5px;'>"
+        string += f"<h3 style='margin-bottom: 0px'>{self.title}</h3>"
+        string += f"<b>{self.arena if self.arena else "No Specified Location"}</b>"
+        string += f"<p>{self.date}, {self.time}</p> "
+        string += f"<p>{self.duration}, {self.calibre}</p></div>"
+        return string
---
+++ notify.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.message import EmailMessage
+from data import mock
+from parser import htmlparser
+
+email_address = 'yarnogrenier@gmail.com'
+email_password = 'bdda njwx izjq dzuk'  # Use your App Password
+
+def notify(games) :
+    msg = EmailMessage()
+    msg['Subject'] = "New Upcoming Games"
+    msg['From'] = email_address
+    msg['To'] = 'yarno@hotmail.ca'
+    content = 'Your games this week:\n' + "\n".join([str(game) for game in games])
+    msg.set_content(content)  # fallback
+    alt = f"""
+        <html>
+        <body style='background-color: skyblue'>
+            {"".join([game.to_html() for game in games])}
+        </body>
+        </html>
+        """
+    msg.add_alternative(alt, subtype='html')
+    print(alt)
+
+    # Connect and send
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_address, email_password)
+        smtp.send_message(msg)
+
+    print("Email sent!")
+
+games = htmlparser(mock)
+notify(games)
+
+
