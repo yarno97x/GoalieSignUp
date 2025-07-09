eeaeb1102e46f075797e37b2fe05ae34a08a2972
Yarno Grenier <127676205+yarno97x@users.noreply.github.com>
2025-07-09 18:06:45 -0400

    Modified tests to include edge cases

--- test_s.py
+++ test_s.py
@@ -17,11 +17,25 @@ def test_check_calibre() :
 def test_check_date() :
     pass
 
-def test_convert_time() :
-    assert converttime("9:00PM") == (9+12)*60
-    assert converttime("6:00AM") == 6*60
-    assert converttime("11:00PM") == (11+12)*60
+def test_convert_time_simple_format() :
+    assert converttime("9:00PM") == (9 + 12) * 60
+    assert converttime("6:00AM") == 6 * 60
+    assert converttime("11:00PM") == (11 + 12) * 60
 
-def test_check_heure() :
+def test_convert_time_twelve_pm() :
+    assert converttime("12:21PM") == 12 * 60 + 21
+
+def test_convert_time_twelve_am() :
+    assert converttime("12:36AM") == 36
+
+def test_check_heure_simple_format() :
     assert not checkheure("9:00PM", "7:00PM")
     assert checkheure("11:00AM", "2:00PM")
+
+def test_check_heure_twelve_am() :
+    assert checkheure("12:00AM", "11:00AM")
+    assert checkheure("12:00AM", "11:00PM")
+
+def test_check_heure_twelve_pm() :
+    assert not checkheure("12:00PM", "11:00AM")
+    assert checkheure("12:00PM", "11:00PM")
